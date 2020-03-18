54c87bc1b3b244c76dd278d10022cd40c4803b41
nefurenn <nefurenn@hotmail.com>
2020-03-19 05:37:39 +0800

    Update config

--- eqfr.sh
+++ eqfr.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-str=$(pm2 list | grep eqfr)
-if [[ -n $str  ]]; then
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+ps_eqfr=$(pm2 list | grep eqfr)
+if [[ -n $ps_eqfr  ]]; then
 	pm2 delete eqfr
 fi
 cd /root/eqfr
-npm install
+npm install --no-audit
 pm2 start "node app.js" --name eqfr 
